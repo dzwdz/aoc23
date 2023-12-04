d9ba2eb63bb3ab7d76a96c391ea652c5a8655d15
dzwdz <kg67199@gmail.com>
2023-12-04 13:12:43 +0100

    4.2

--- 4.py
+++ 4.py
@@ -2,13 +2,18 @@ import sys
 lines = sys.stdin.readlines()
 
 tally1 = 0
-for line in lines:
-	i, line = line.split(': ')
-	i = int(i.split()[-1])
+counts = [1] * len(lines)
+for i, line in enumerate(lines):
+	_, line = line.split(': ')
+	#i = int(i.split()[-1])
 	wins, mine = line.split('|')
 	wins = [int(n) for n in wins.split()]
 	mine = [int(n) for n in mine.split()]
 	matches = [n for n in mine if n in wins]
 	if matches:
 		tally1 += 2 ** (len(matches) - 1)
+	for j in range(len(matches)):
+		if i+j+1 < len(counts):
+			counts[i+j+1] += counts[i]
 print(tally1)
+print(sum(counts))
