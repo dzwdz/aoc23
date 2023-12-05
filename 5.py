dbe65f8ee34056da3d7ec4a52d54be818eca7c24
dzwdz <kg67199@gmail.com>
2023-12-05 11:40:52 +0100

    5.2

--- 5.py
+++ 5.py
@@ -1,4 +1,5 @@
 import sys
+from math import inf
 
 def listsplit(a, pred):
 	res = [[]]
@@ -37,4 +38,59 @@ def part1():
 		ns = [trans(n, m) for n in ns]
 	return min(ns)
 
+def part2():
+	# (start, end)
+	cur = []
+	for i in range(0, len(seeds), 2):
+		cur.append((seeds[i], seeds[i]+seeds[i+1]))
+	def overlap(a, b):
+		r = (max(a[0], b[0]), min(a[1], b[1]))
+		if r[0] < r[1]: return r
+		return None
+	def subtract(a, b):
+		r = (a[0], min(a[1], b[0]))
+		if r[0] < r[1]: yield r
+		r = (max(a[0], b[1]), a[1])
+		if r[0] < r[1]: yield r
+	def trans(r, m):
+		remainder = [r]
+		for src, dst, size in m:
+			# U
+			rr = overlap(r, (dst, dst+size))
+			if rr:
+				off = src-dst
+				rr = (rr[0]+off, rr[1]+off)
+				yield rr
+
+			# \
+			oldrem = remainder
+			remainder = []
+			for rem in oldrem:
+				remainder += subtract(rem, (dst, dst+size))
+		for rem in remainder:
+			yield rem
+	
+	def simplify(a):
+		a.sort()
+		res = []
+		i = 0
+		while i < len(a):
+			r = a[i]
+			i += 1
+			while i < len(a) and r[1] >= a[i][0]:
+				r = (r[0], max(r[1], a[i][1]))
+				i += 1
+			res.append(r)
+		return res
+	
+	for m in maps:
+		last = cur
+		cur = []
+		for r in last:
+			for out in trans(r, m):
+				cur.append(out)
+		cur = simplify(cur)
+	return cur[0][0]
+
 print(part1())
+print(part2())
